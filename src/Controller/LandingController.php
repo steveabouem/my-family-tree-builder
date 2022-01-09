5200d0d784c5a009148469fb9236f7b3b6f6137b
Steve Abouem <steveabouem@gmail.com>
2022-01-09 08:19:59 -0500

    extend abstract controller

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -2,19 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
 * @Route("/")
 */
-class LandingController
+class LandingController extends AbstractController
 {
     /**
     * @Route("/", name="home_page", methods={"GET", "HEAD"})
     */
     public function show(): Response
     {
-        return new Response ('<html><body>Temp</body></html>');
+        return new Response ('<html><body>Landing</body></html>');
     }
 }
