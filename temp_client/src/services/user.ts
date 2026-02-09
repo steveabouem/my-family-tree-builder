da037dab44cc543021eef429e1eebd200e1db645
steveabouem <sabouem@c2.biz>
2026-02-09 16:32:38 -0500

    reorganized api folder in front

--- temp_client/src/services/user.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { baseUrl } from ".";
-
-export const getExtendedFamilies = async (id: number): Promise<any> => {
-  // get all families of the same level in the tree (families grouped by parents and children' spouses' parents) for a given user
-  const results = axios.get(`${baseUrl}/users/${id}/extended`);
-  return results;
-}
--- temp_client/src/services/v2/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './authV2';
-export * from './familyTreeV2';
-export * from './userV2';
-export * from './sessionV2';
-
